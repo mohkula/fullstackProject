54026d12547eae0014d6efc55ad04c283bb21f62
Mohkula <matias.himanen@helsinki.fi>
2022-07-14 13:42:54 +0300

    secure password entry added

--- gamifyre/src/components/SignUpForm.jsx
+++ gamifyre/src/components/SignUpForm.jsx
@@ -8,8 +8,8 @@ const SignUpForm = ({ onSubmit }) => {
   return (
     <View style={styles.container}>
       <FormikTextInput name='username' placeholder="Username"/>
-      <FormikTextInput name='password' placeholder="Password"/>
-      <FormikTextInput name='passwordConfirmation' placeholder="Password confirmation"/>
+      <FormikTextInput secureTextEntry={true} name='password' placeholder="Password"/>
+      <FormikTextInput secureTextEntry={true} name='passwordConfirmation' placeholder="Password confirmation"/>
       <Pressable style={styles.signinContainer} onPress={onSubmit}>
         <Text style={{ textAlign: 'center'}} color="white" fontWeight='bold' fontSize="subheading">Sign Up</Text>
       </Pressable>
