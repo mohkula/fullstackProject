926da8fab742bd14c6054a583e1fc223bbdf2498
Mohkula <matias.himanen@helsinki.fi>
2022-07-20 17:08:29 +0300

    newGoal form validation to allow only increments that divide the steps equally

--- gamifyre/src/components/NewGoal.jsx
+++ gamifyre/src/components/NewGoal.jsx
@@ -13,18 +13,32 @@ import NewGoalForm from "./NewGoalForm";
 const initialValues = {
   name: "",
   description: "",
-  steps: 0,
-  increments: 0,
+  steps: "",
+  increments: "",
   
 };
+
+let helper = 1
 
 const validationSchema = yup.object().shape({
   name: yup.string().min(1).max(30).required("name is required"),
   steps: yup.number().typeError('steps must be a number')
   .positive('steps must be positive')
-  .required('number of steps is required'),
+  .required('number of steps is required')
+  .test('divisible', 'steps must be divisible by increments', 
+  function(value){
+
+    helper = value
+return true   
+  }),
   increments: yup.number().typeError('increments must be a number')
   .positive('increments must be positive')
+  .test('divisible', 'steps must be divisible by increments', 
+  function(value){
+
+    
+return helper % value === 0
+  })
   .required('number of increments is required')
 });
 
